4a03bbf15e14add96983af8fe386e11b6eb2c867
xiangyong <cc.123>
2018-05-16 15:17:12 +0800

    #爬去豆瓣电影top最热的电影，

--- DouBanMovie/spider.py
+++ DouBanMovie/spider.py
@@ -1,10 +1,54 @@
 import re
 import requests
+import  json
 
 
-def get_pages():
-    print(1)
+def get_one_pages(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.RequestException:
+        return None
+
+def parse_one_page(html):
+    pattern = re.compile('<li.*?class="item".*?<em.*?>(.*?)</em>.*?<img.*?src="(.*?)".*?>'
+                         + '*?<span.*?>(.*?)</span>.*?<span.*?>(.*?)'
+                         + '</span>.*?<span.*?>(.*?)</span>.*?bd.*?<p.*?>(.*?)<br>(.*?)</p>'
+                         + '.*?star.*?<span.*?property="v:average">(.*?)</span>.*?<span>'
+                         + '(.*?)</span>.*?quote.*?<span.*?>(.*?)</span>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+           'top': item[0],
+           'movie': item[2],
+           'score': item[7],
+           'sum': item[8],
+           'director': re.split('&nbsp;', item[5].strip()[4:])[0],
+           'actor': re.split('&nbsp;', item[5].strip()[4:])[3],
+           'time': re.split('&nbsp;', item[6].strip())[0],
+           'type': re.split('&nbsp;', item[6].strip())[4],
+           'country': re.split('&nbsp;', item[6].strip())[2],
+           'image': item[1]
+
+        }
+
+
+def write_to_file(content):
+    with open('movies.text', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False)+'\n')
+        f.close()
+
+
+def main():
+    url = 'https://movie.douban.com/top250'
+    html = get_one_pages(url)
+    for item in parse_one_page(html):
+        write_to_file(item)
 
 
 if __name__ == '__main__':
-      get_pages()
+    main()
+
+
