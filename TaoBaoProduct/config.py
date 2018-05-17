d4ed2f20ea805fe74341098298e74c04610ff764
xiangyong <cc.123>
2018-05-17 21:47:14 +0800

    #抓取淘宝千级sku存储到MONGODB后台无无浏览器抓取`

---
+++ TaoBaoProduct/config.py
@@ -0,0 +1,7 @@
+MANGO_URL = 'mongodb://localhost:27017/'
+MANGO_DB = 'taoBao'
+MANGO_TABLE = 'taoBao'
+GROUP_START = 1
+GROUP_END = 20
+KEYWORD = '美食'
+SERVICE_ARGEs =['--load-images=false', '--disk-cache=true']
--- TaoBaoProduct/spider.py
+++ TaoBaoProduct/spider.py
@@ -5,14 +5,19 @@ from selenium.webdriver.support import expected_conditions as ec
 import re
 from pyquery import PyQuery as Pq
 from selenium.common import exceptions
+from TaoBaoProduct import config
+import pymongo
 
+client = pymongo.MongoClient(config.MANGO_URL)
+db = client[config.MANGO_DB]
 
-
-browser = webdriver.Chrome()
+browser = webdriver.PhantomJS(service_args=config.SERVICE_ARGEs)
 wait_ = WebDriverWait(browser, 10)
+browser.set_window_size(1400, 900)
 
 
 def search():
+    print('正在搜索')
     try:
         browser.get('https://www.taobao.com/')
         input_ = wait_.until(
@@ -20,7 +25,7 @@ def search():
         )
         submit = wait_.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > '
                                                                           'div.search-button > button')))
-        input_.send_keys('美食')
+        input_.send_keys(config.KEYWORD)
         submit.click()
         total = wait_.until(ec.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-pager > div > "
                                                                              "div > div > div.total")))
@@ -31,6 +36,7 @@ def search():
 
 
 def next_page(page_number):
+    print('正在翻页')
     try:
         input_ = wait_.until(
             ec.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > input"))
@@ -64,17 +70,32 @@ def get_products():
                 'image': item.find('.pic .img').attr('src'),
 
             }
+            save_to_mongodb(product)
             print(product)
     except exceptions.TimeoutException:
         return get_products()
 
 
+def save_to_mongodb(result):
+    try:
+        if db[config.MANGO_TABLE].insert(result):
+            print('存储mongodb成功', result)
+    except Exception:
+        print('存储mongodb失败', result)
+
+
 def main():
-    total = search()
-    patter = re.compile('\d+', re.S)
-    total = int(re.search(patter, total).group())
-    for i in range(2, total+1):
-        next_page(i)
+    try:
+        total = search()
+        patter = re.compile('\d+', re.S)
+        total = int(re.search(patter, total).group())
+        for i in range(2, total+1):
+            next_page(i)
+        browser.close()
+    except Exception:
+        print('出错啦')
+    finally:
+        browser.close()
 
 
 if __name__ == '__main__':
